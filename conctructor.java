077e4ba9aff0a313743b6d9fe051feea08e4036c
diya-khatri <125243554+diya-khatri@users.noreply.github.com>
2023-02-13 14:57:45 +0530

    Add files via upload

---
+++ conctructor.java
@@ -0,0 +1,11 @@
+class conctructor {
+   int  x;
+    conctructor()
+   {
+    x=5;
+   }
+    public static void main(String[] args) {
+        conctructor myObj=new conctructor();
+        System.out.println(myObj.x);
+    }
+}
